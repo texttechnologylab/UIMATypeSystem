21f50d58e396a345152eefa8a1c39d27f5203335
abrami <abrami@em.uni-frankfurt.de>
2021-02-19 19:20:15 +0100

    Add CorrectionToken

---
+++ src/main/java/org/texttechnologylab/annotation/token/Correction.java
@@ -0,0 +1,101 @@
+
+
+/* First created by JCasGen Fri Feb 19 19:19:05 CET 2021 */
+package org.texttechnologylab.annotation.token;
+
+import org.apache.uima.jcas.JCas; 
+import org.apache.uima.jcas.JCasRegistry;
+import org.apache.uima.jcas.cas.TOP_Type;
+
+import org.apache.uima.jcas.tcas.Annotation;
+
+
+/** 
+ * Updated by JCasGen Fri Feb 19 19:19:05 CET 2021
+ * XML source: /home/gabrami/Projects/UIMATypeSystem/src/main/resources/desc/type/TextTechnologyAnnotation.xml
+ * @generated */
+public class Correction extends Annotation {
+  /** @generated
+   * @ordered 
+   */
+  @SuppressWarnings ("hiding")
+  public final static int typeIndexID = JCasRegistry.register(Correction.class);
+  /** @generated
+   * @ordered 
+   */
+  @SuppressWarnings ("hiding")
+  public final static int type = typeIndexID;
+  /** @generated
+   * @return index of the type  
+   */
+  @Override
+  public              int getTypeIndexID() {return typeIndexID;}
+ 
+  /** Never called.  Disable default constructor
+   * @generated */
+  protected Correction() {/* intentionally empty block */}
+    
+  /** Internal - constructor used by generator 
+   * @generated
+   * @param addr low level Feature Structure reference
+   * @param type the type of this Feature Structure 
+   */
+  public Correction(int addr, TOP_Type type) {
+    super(addr, type);
+    readObject();
+  }
+  
+  /** @generated
+   * @param jcas JCas to which this Feature Structure belongs 
+   */
+  public Correction(JCas jcas) {
+    super(jcas);
+    readObject();   
+  } 
+
+  /** @generated
+   * @param jcas JCas to which this Feature Structure belongs
+   * @param begin offset to the begin spot in the SofA
+   * @param end offset to the end spot in the SofA 
+  */  
+  public Correction(JCas jcas, int begin, int end) {
+    super(jcas);
+    setBegin(begin);
+    setEnd(end);
+    readObject();
+  }   
+
+  /** 
+   * <!-- begin-user-doc -->
+   * Write your own initialization here
+   * <!-- end-user-doc -->
+   *
+   * @generated modifiable 
+   */
+  private void readObject() {/*default - does nothing empty block */}
+     
+ 
+    
+  //*--------------*
+  //* Feature: value
+
+  /** getter for value - gets 
+   * @generated
+   * @return value of the feature 
+   */
+  public String getValue() {
+    if (Correction_Type.featOkTst && ((Correction_Type)jcasType).casFeat_value == null)
+      jcasType.jcas.throwFeatMissing("value", "org.texttechnologylab.annotation.token.Correction");
+    return jcasType.ll_cas.ll_getStringValue(addr, ((Correction_Type)jcasType).casFeatCode_value);}
+    
+  /** setter for value - sets  
+   * @generated
+   * @param v value to set into the feature 
+   */
+  public void setValue(String v) {
+    if (Correction_Type.featOkTst && ((Correction_Type)jcasType).casFeat_value == null)
+      jcasType.jcas.throwFeatMissing("value", "org.texttechnologylab.annotation.token.Correction");
+    jcasType.ll_cas.ll_setStringValue(addr, ((Correction_Type)jcasType).casFeatCode_value, v);}    
+  }
+
+    
