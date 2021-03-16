6ef93e0ce7ce323ff79913f9d057b82a6d295d12
abrami <abrami@em.uni-frankfurt.de>
2021-03-16 11:39:13 +0100

    AnnotationBracket

---
+++ src/main/java/org/texttechnologylab/annotation/AnnotationBracket_Type.java
@@ -0,0 +1,39 @@
+
+/* First created by JCasGen Tue Mar 16 11:38:05 CET 2021 */
+package org.texttechnologylab.annotation;
+
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.JCasRegistry;
+import org.apache.uima.cas.impl.TypeImpl;
+import org.apache.uima.cas.Type;
+import org.apache.uima.jcas.tcas.Annotation_Type;
+
+/** 
+ * Updated by JCasGen Tue Mar 16 11:38:05 CET 2021
+ * @generated */
+public class AnnotationBracket_Type extends Annotation_Type {
+  /** @generated */
+  @SuppressWarnings ("hiding")
+  public final static int typeIndexID = AnnotationBracket.typeIndexID;
+  /** @generated 
+     @modifiable */
+  @SuppressWarnings ("hiding")
+  public final static boolean featOkTst = JCasRegistry.getFeatOkTst("org.texttechnologylab.annotation.AnnotationBracket");
+
+
+
+  /** initialize variables to correspond with Cas Type and Features
+	 * @generated
+	 * @param jcas JCas
+	 * @param casType Type 
+	 */
+  public AnnotationBracket_Type(JCas jcas, Type casType) {
+    super(jcas, casType);
+    casImpl.getFSClassRegistry().addGeneratorForType((TypeImpl)this.casType, getFSGenerator());
+
+  }
+}
+
+
+
+    
