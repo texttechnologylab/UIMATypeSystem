e69ac5cf54a17d8da7bd02b1abc65d44ca5bc3f2
Giuseppe Abrami <abrami@em.uni-frankfurt.de>
2023-06-02 16:43:17 +0200

    new Type Paraphrase

---
+++ src/main/java/org/texttechnologylab/annotation/Paraphrase.java
@@ -0,0 +1,101 @@
+
+
+/* First created by JCasGen Fri Jun 02 16:42:17 CEST 2023 */
+package org.texttechnologylab.annotation;
+
+import org.apache.uima.jcas.JCas; 
+import org.apache.uima.jcas.JCasRegistry;
+import org.apache.uima.jcas.cas.TOP_Type;
+
+import org.apache.uima.jcas.tcas.Annotation;
+
+
+/** 
+ * Updated by JCasGen Fri Jun 02 16:42:17 CEST 2023
+ * XML source: /home/staff_homes/abrami/Projects/GitProjects/UIMATypeSystem/src/main/resources/desc/type/TextTechnologyParaphrase.xml
+ * @generated */
+public class Paraphrase extends Annotation {
+  /** @generated
+   * @ordered 
+   */
+  @SuppressWarnings ("hiding")
+  public final static int typeIndexID = JCasRegistry.register(Paraphrase.class);
+  /** @generated
+   * @ordered 
+   */
+  @SuppressWarnings ("hiding")
+  public final static int type = typeIndexID;
+  /** @generated
+   * @return index of the type  
+   */
+  @Override
+  public              int getTypeIndexID() {return typeIndexID;}
+ 
+  /** Never called.  Disable default constructor
+   * @generated */
+  protected Paraphrase() {/* intentionally empty block */}
+    
+  /** Internal - constructor used by generator 
+   * @generated
+   * @param addr low level Feature Structure reference
+   * @param type the type of this Feature Structure 
+   */
+  public Paraphrase(int addr, TOP_Type type) {
+    super(addr, type);
+    readObject();
+  }
+  
+  /** @generated
+   * @param jcas JCas to which this Feature Structure belongs 
+   */
+  public Paraphrase(JCas jcas) {
+    super(jcas);
+    readObject();   
+  } 
+
+  /** @generated
+   * @param jcas JCas to which this Feature Structure belongs
+   * @param begin offset to the begin spot in the SofA
+   * @param end offset to the end spot in the SofA 
+  */  
+  public Paraphrase(JCas jcas, int begin, int end) {
+    super(jcas);
+    setBegin(begin);
+    setEnd(end);
+    readObject();
+  }   
+
+  /** 
+   * <!-- begin-user-doc -->
+   * Write your own initialization here
+   * <!-- end-user-doc -->
+   *
+   * @generated modifiable 
+   */
+  private void readObject() {/*default - does nothing empty block */}
+     
+ 
+    
+  //*--------------*
+  //* Feature: value
+
+  /** getter for value - gets 
+   * @generated
+   * @return value of the feature 
+   */
+  public String getValue() {
+    if (Paraphrase_Type.featOkTst && ((Paraphrase_Type)jcasType).casFeat_value == null)
+      jcasType.jcas.throwFeatMissing("value", "org.texttechnologylab.annotation.Paraphrase");
+    return jcasType.ll_cas.ll_getStringValue(addr, ((Paraphrase_Type)jcasType).casFeatCode_value);}
+    
+  /** setter for value - sets  
+   * @generated
+   * @param v value to set into the feature 
+   */
+  public void setValue(String v) {
+    if (Paraphrase_Type.featOkTst && ((Paraphrase_Type)jcasType).casFeat_value == null)
+      jcasType.jcas.throwFeatMissing("value", "org.texttechnologylab.annotation.Paraphrase");
+    jcasType.ll_cas.ll_setStringValue(addr, ((Paraphrase_Type)jcasType).casFeatCode_value, v);}    
+  }
+
+    
---
+++ src/main/java/org/texttechnologylab/annotation/Paraphrase_Type.java
@@ -0,0 +1,69 @@
+
+/* First created by JCasGen Fri Jun 02 16:42:17 CEST 2023 */
+package org.texttechnologylab.annotation;
+
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.JCasRegistry;
+import org.apache.uima.cas.impl.TypeImpl;
+import org.apache.uima.cas.Type;
+import org.apache.uima.cas.impl.FeatureImpl;
+import org.apache.uima.cas.Feature;
+import org.apache.uima.jcas.tcas.Annotation_Type;
+
+/** 
+ * Updated by JCasGen Fri Jun 02 16:42:17 CEST 2023
+ * @generated */
+public class Paraphrase_Type extends Annotation_Type {
+  /** @generated */
+  @SuppressWarnings ("hiding")
+  public final static int typeIndexID = Paraphrase.typeIndexID;
+  /** @generated 
+     @modifiable */
+  @SuppressWarnings ("hiding")
+  public final static boolean featOkTst = JCasRegistry.getFeatOkTst("org.texttechnologylab.annotation.Paraphrase");
+ 
+  /** @generated */
+  final Feature casFeat_value;
+  /** @generated */
+  final int     casFeatCode_value;
+  /** @generated
+   * @param addr low level Feature Structure reference
+   * @return the feature value 
+   */ 
+  public String getValue(int addr) {
+        if (featOkTst && casFeat_value == null)
+      jcas.throwFeatMissing("value", "org.texttechnologylab.annotation.Paraphrase");
+    return ll_cas.ll_getStringValue(addr, casFeatCode_value);
+  }
+  /** @generated
+   * @param addr low level Feature Structure reference
+   * @param v value to set 
+   */    
+  public void setValue(int addr, String v) {
+        if (featOkTst && casFeat_value == null)
+      jcas.throwFeatMissing("value", "org.texttechnologylab.annotation.Paraphrase");
+    ll_cas.ll_setStringValue(addr, casFeatCode_value, v);}
+    
+  
+
+
+
+  /** initialize variables to correspond with Cas Type and Features
+	 * @generated
+	 * @param jcas JCas
+	 * @param casType Type 
+	 */
+  public Paraphrase_Type(JCas jcas, Type casType) {
+    super(jcas, casType);
+    casImpl.getFSClassRegistry().addGeneratorForType((TypeImpl)this.casType, getFSGenerator());
+
+ 
+    casFeat_value = jcas.getRequiredFeatureDE(casType, "value", "uima.cas.String", featOkTst);
+    casFeatCode_value  = (null == casFeat_value) ? JCas.INVALID_FEATURE_CODE : ((FeatureImpl)casFeat_value).getCode();
+
+  }
+}
+
+
+
+    
