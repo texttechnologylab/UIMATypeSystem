9763f50f801c56edfc20bde0baee34f3a67fcf6f
Giuseppe Abrami <abrami@em.uni-frankfurt.de>
2024-01-10 12:00:43 +0100

    build Core.Category

---
+++ src/main/java/org/texttechnologylab/annotation/core/Category.java
@@ -0,0 +1,145 @@
+
+
+/* First created by JCasGen Wed Jan 10 11:59:30 CET 2024 */
+package org.texttechnologylab.annotation.core;
+
+import org.apache.uima.jcas.JCas; 
+import org.apache.uima.jcas.JCasRegistry;
+import org.apache.uima.jcas.cas.TOP_Type;
+
+import org.apache.uima.jcas.tcas.Annotation;
+
+
+/** Generic category type
+ * Updated by JCasGen Wed Jan 10 11:59:30 CET 2024
+ * XML source: /home/staff_homes/abrami/Projects/GitProjects/UIMATypeSystem/src/main/resources/desc/type/Core.xml
+ * @generated */
+public class Category extends Annotation {
+  /** @generated
+   * @ordered 
+   */
+  @SuppressWarnings ("hiding")
+  public final static int typeIndexID = JCasRegistry.register(Category.class);
+  /** @generated
+   * @ordered 
+   */
+  @SuppressWarnings ("hiding")
+  public final static int type = typeIndexID;
+  /** @generated
+   * @return index of the type  
+   */
+  @Override
+  public              int getTypeIndexID() {return typeIndexID;}
+ 
+  /** Never called.  Disable default constructor
+   * @generated */
+  protected Category() {/* intentionally empty block */}
+    
+  /** Internal - constructor used by generator 
+   * @generated
+   * @param addr low level Feature Structure reference
+   * @param type the type of this Feature Structure 
+   */
+  public Category(int addr, TOP_Type type) {
+    super(addr, type);
+    readObject();
+  }
+  
+  /** @generated
+   * @param jcas JCas to which this Feature Structure belongs 
+   */
+  public Category(JCas jcas) {
+    super(jcas);
+    readObject();   
+  } 
+
+  /** @generated
+   * @param jcas JCas to which this Feature Structure belongs
+   * @param begin offset to the begin spot in the SofA
+   * @param end offset to the end spot in the SofA 
+  */  
+  public Category(JCas jcas, int begin, int end) {
+    super(jcas);
+    setBegin(begin);
+    setEnd(end);
+    readObject();
+  }   
+
+  /** 
+   * <!-- begin-user-doc -->
+   * Write your own initialization here
+   * <!-- end-user-doc -->
+   *
+   * @generated modifiable 
+   */
+  private void readObject() {/*default - does nothing empty block */}
+     
+ 
+    
+  //*--------------*
+  //* Feature: key
+
+  /** getter for key - gets 
+   * @generated
+   * @return value of the feature 
+   */
+  public String getKey() {
+    if (Category_Type.featOkTst && ((Category_Type)jcasType).casFeat_key == null)
+      jcasType.jcas.throwFeatMissing("key", "org.texttechnologylab.annotation.core.Category");
+    return jcasType.ll_cas.ll_getStringValue(addr, ((Category_Type)jcasType).casFeatCode_key);}
+    
+  /** setter for key - sets  
+   * @generated
+   * @param v value to set into the feature 
+   */
+  public void setKey(String v) {
+    if (Category_Type.featOkTst && ((Category_Type)jcasType).casFeat_key == null)
+      jcasType.jcas.throwFeatMissing("key", "org.texttechnologylab.annotation.core.Category");
+    jcasType.ll_cas.ll_setStringValue(addr, ((Category_Type)jcasType).casFeatCode_key, v);}    
+   
+    
+  //*--------------*
+  //* Feature: value
+
+  /** getter for value - gets 
+   * @generated
+   * @return value of the feature 
+   */
+  public String getValue() {
+    if (Category_Type.featOkTst && ((Category_Type)jcasType).casFeat_value == null)
+      jcasType.jcas.throwFeatMissing("value", "org.texttechnologylab.annotation.core.Category");
+    return jcasType.ll_cas.ll_getStringValue(addr, ((Category_Type)jcasType).casFeatCode_value);}
+    
+  /** setter for value - sets  
+   * @generated
+   * @param v value to set into the feature 
+   */
+  public void setValue(String v) {
+    if (Category_Type.featOkTst && ((Category_Type)jcasType).casFeat_value == null)
+      jcasType.jcas.throwFeatMissing("value", "org.texttechnologylab.annotation.core.Category");
+    jcasType.ll_cas.ll_setStringValue(addr, ((Category_Type)jcasType).casFeatCode_value, v);}    
+   
+    
+  //*--------------*
+  //* Feature: description
+
+  /** getter for description - gets 
+   * @generated
+   * @return value of the feature 
+   */
+  public String getDescription() {
+    if (Category_Type.featOkTst && ((Category_Type)jcasType).casFeat_description == null)
+      jcasType.jcas.throwFeatMissing("description", "org.texttechnologylab.annotation.core.Category");
+    return jcasType.ll_cas.ll_getStringValue(addr, ((Category_Type)jcasType).casFeatCode_description);}
+    
+  /** setter for description - sets  
+   * @generated
+   * @param v value to set into the feature 
+   */
+  public void setDescription(String v) {
+    if (Category_Type.featOkTst && ((Category_Type)jcasType).casFeat_description == null)
+      jcasType.jcas.throwFeatMissing("description", "org.texttechnologylab.annotation.core.Category");
+    jcasType.ll_cas.ll_setStringValue(addr, ((Category_Type)jcasType).casFeatCode_description, v);}    
+  }
+
+    
---
+++ src/main/java/org/texttechnologylab/annotation/core/Category_Type.java
@@ -0,0 +1,125 @@
+
+/* First created by JCasGen Wed Jan 10 11:59:30 CET 2024 */
+package org.texttechnologylab.annotation.core;
+
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.JCasRegistry;
+import org.apache.uima.cas.impl.TypeImpl;
+import org.apache.uima.cas.Type;
+import org.apache.uima.cas.impl.FeatureImpl;
+import org.apache.uima.cas.Feature;
+import org.apache.uima.jcas.tcas.Annotation_Type;
+
+/** Generic category type
+ * Updated by JCasGen Wed Jan 10 11:59:30 CET 2024
+ * @generated */
+public class Category_Type extends Annotation_Type {
+  /** @generated */
+  @SuppressWarnings ("hiding")
+  public final static int typeIndexID = Category.typeIndexID;
+  /** @generated 
+     @modifiable */
+  @SuppressWarnings ("hiding")
+  public final static boolean featOkTst = JCasRegistry.getFeatOkTst("org.texttechnologylab.annotation.core.Category");
+ 
+  /** @generated */
+  final Feature casFeat_key;
+  /** @generated */
+  final int     casFeatCode_key;
+  /** @generated
+   * @param addr low level Feature Structure reference
+   * @return the feature value 
+   */ 
+  public String getKey(int addr) {
+        if (featOkTst && casFeat_key == null)
+      jcas.throwFeatMissing("key", "org.texttechnologylab.annotation.core.Category");
+    return ll_cas.ll_getStringValue(addr, casFeatCode_key);
+  }
+  /** @generated
+   * @param addr low level Feature Structure reference
+   * @param v value to set 
+   */    
+  public void setKey(int addr, String v) {
+        if (featOkTst && casFeat_key == null)
+      jcas.throwFeatMissing("key", "org.texttechnologylab.annotation.core.Category");
+    ll_cas.ll_setStringValue(addr, casFeatCode_key, v);}
+    
+  
+ 
+  /** @generated */
+  final Feature casFeat_value;
+  /** @generated */
+  final int     casFeatCode_value;
+  /** @generated
+   * @param addr low level Feature Structure reference
+   * @return the feature value 
+   */ 
+  public String getValue(int addr) {
+        if (featOkTst && casFeat_value == null)
+      jcas.throwFeatMissing("value", "org.texttechnologylab.annotation.core.Category");
+    return ll_cas.ll_getStringValue(addr, casFeatCode_value);
+  }
+  /** @generated
+   * @param addr low level Feature Structure reference
+   * @param v value to set 
+   */    
+  public void setValue(int addr, String v) {
+        if (featOkTst && casFeat_value == null)
+      jcas.throwFeatMissing("value", "org.texttechnologylab.annotation.core.Category");
+    ll_cas.ll_setStringValue(addr, casFeatCode_value, v);}
+    
+  
+ 
+  /** @generated */
+  final Feature casFeat_description;
+  /** @generated */
+  final int     casFeatCode_description;
+  /** @generated
+   * @param addr low level Feature Structure reference
+   * @return the feature value 
+   */ 
+  public String getDescription(int addr) {
+        if (featOkTst && casFeat_description == null)
+      jcas.throwFeatMissing("description", "org.texttechnologylab.annotation.core.Category");
+    return ll_cas.ll_getStringValue(addr, casFeatCode_description);
+  }
+  /** @generated
+   * @param addr low level Feature Structure reference
+   * @param v value to set 
+   */    
+  public void setDescription(int addr, String v) {
+        if (featOkTst && casFeat_description == null)
+      jcas.throwFeatMissing("description", "org.texttechnologylab.annotation.core.Category");
+    ll_cas.ll_setStringValue(addr, casFeatCode_description, v);}
+    
+  
+
+
+
+  /** initialize variables to correspond with Cas Type and Features
+	 * @generated
+	 * @param jcas JCas
+	 * @param casType Type 
+	 */
+  public Category_Type(JCas jcas, Type casType) {
+    super(jcas, casType);
+    casImpl.getFSClassRegistry().addGeneratorForType((TypeImpl)this.casType, getFSGenerator());
+
+ 
+    casFeat_key = jcas.getRequiredFeatureDE(casType, "key", "uima.cas.String", featOkTst);
+    casFeatCode_key  = (null == casFeat_key) ? JCas.INVALID_FEATURE_CODE : ((FeatureImpl)casFeat_key).getCode();
+
+ 
+    casFeat_value = jcas.getRequiredFeatureDE(casType, "value", "uima.cas.String", featOkTst);
+    casFeatCode_value  = (null == casFeat_value) ? JCas.INVALID_FEATURE_CODE : ((FeatureImpl)casFeat_value).getCode();
+
+ 
+    casFeat_description = jcas.getRequiredFeatureDE(casType, "description", "uima.cas.String", featOkTst);
+    casFeatCode_description  = (null == casFeat_description) ? JCas.INVALID_FEATURE_CODE : ((FeatureImpl)casFeat_description).getCode();
+
+  }
+}
+
+
+
+    
