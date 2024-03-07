27b6853e8a38233fc7e2711c630264e825d10ff0
Mevluet Bagci <bagci@em.uni-frankfurt.de>
2024-03-07 18:18:58 +0100

    ADD FactChecking TypeSystem

---
+++ src/main/java/org/texttechnologylab/annotation/Claim.java
@@ -0,0 +1,178 @@
+
+
+   
+/* Apache UIMA v3 - First created by JCasGen Thu Mar 07 11:48:13 CET 2024 */
+
+package org.texttechnologylab.annotation;
+ 
+
+import java.lang.invoke.CallSite;
+import java.lang.invoke.MethodHandle;
+
+import org.apache.uima.cas.impl.CASImpl;
+import org.apache.uima.cas.impl.TypeImpl;
+import org.apache.uima.cas.impl.TypeSystemImpl;
+import org.apache.uima.jcas.JCas; 
+import org.apache.uima.jcas.JCasRegistry;
+
+
+import org.apache.uima.jcas.cas.FSArray;
+import org.apache.uima.jcas.tcas.Annotation;
+
+
+/** One Claim for different facts
+ * Updated by JCasGen Thu Mar 07 11:48:13 CET 2024
+ * XML source: /home/bagci/projects/UIMATypesystem-uima-3/target/jcasgen/typesystem.xml
+ * @generated */
+public class Claim extends Annotation {
+ 
+  /** @generated
+   * @ordered 
+   */
+  @SuppressWarnings ("hiding")
+  public final static String _TypeName = "org.texttechnologylab.annotation.Claim";
+  
+  /** @generated
+   * @ordered 
+   */
+  @SuppressWarnings ("hiding")
+  public final static int typeIndexID = JCasRegistry.register(Claim.class);
+  /** @generated
+   * @ordered 
+   */
+  @SuppressWarnings ("hiding")
+  public final static int type = typeIndexID;
+  /** @generated
+   * @return index of the type  
+   */
+  @Override
+  public              int getTypeIndexID() {return typeIndexID;}
+ 
+ 
+  /* *******************
+   *   Feature Offsets *
+   * *******************/ 
+   
+  public final static String _FeatName_value = "value";
+  public final static String _FeatName_Facts = "Facts";
+
+
+  /* Feature Adjusted Offsets */
+  private final static CallSite _FC_value = TypeSystemImpl.createCallSite(Claim.class, "value");
+  private final static MethodHandle _FH_value = _FC_value.dynamicInvoker();
+  private final static CallSite _FC_Facts = TypeSystemImpl.createCallSite(Claim.class, "Facts");
+  private final static MethodHandle _FH_Facts = _FC_Facts.dynamicInvoker();
+
+   
+  /** Never called.  Disable default constructor
+   * @generated */
+  @Deprecated
+  @SuppressWarnings ("deprecation")
+  protected Claim() {/* intentionally empty block */}
+    
+  /** Internal - constructor used by generator 
+   * @generated
+   * @param casImpl the CAS this Feature Structure belongs to
+   * @param type the type of this Feature Structure 
+   */
+  public Claim(TypeImpl type, CASImpl casImpl) {
+    super(type, casImpl);
+    readObject();
+  }
+  
+  /** @generated
+   * @param jcas JCas to which this Feature Structure belongs 
+   */
+  public Claim(JCas jcas) {
+    super(jcas);
+    readObject();   
+  } 
+
+
+  /** @generated
+   * @param jcas JCas to which this Feature Structure belongs
+   * @param begin offset to the begin spot in the SofA
+   * @param end offset to the end spot in the SofA 
+  */  
+  public Claim(JCas jcas, int begin, int end) {
+    super(jcas);
+    setBegin(begin);
+    setEnd(end);
+    readObject();
+  }   
+
+  /** 
+   * <!-- begin-user-doc -->
+   * Write your own initialization here
+   * <!-- end-user-doc -->
+   *
+   * @generated modifiable 
+   */
+  private void readObject() {/*default - does nothing empty block */}
+     
+ 
+    
+  //*--------------*
+  //* Feature: value
+
+  /** getter for value - gets Information of Claim
+   * @generated
+   * @return value of the feature 
+   */
+  public String getValue() { 
+    return _getStringValueNc(wrapGetIntCatchException(_FH_value));
+  }
+    
+  /** setter for value - sets Information of Claim 
+   * @generated
+   * @param v value to set into the feature 
+   */
+  public void setValue(String v) {
+    _setStringValueNfc(wrapGetIntCatchException(_FH_value), v);
+  }    
+    
+   
+    
+  //*--------------*
+  //* Feature: Facts
+
+  /** getter for Facts - gets Set of Fact
+   * @generated
+   * @return value of the feature 
+   */
+  @SuppressWarnings("unchecked")
+  public FSArray<Fact> getFacts() { 
+    return (FSArray<Fact>)(_getFeatureValueNc(wrapGetIntCatchException(_FH_Facts)));
+  }
+    
+  /** setter for Facts - sets Set of Fact 
+   * @generated
+   * @param v value to set into the feature 
+   */
+  public void setFacts(FSArray<Fact> v) {
+    _setFeatureValueNcWj(wrapGetIntCatchException(_FH_Facts), v);
+  }    
+    
+    
+  /** indexed getter for Facts - gets an indexed value - Set of Fact
+   * @generated
+   * @param i index in the array to get
+   * @return value of the element at index i 
+   */
+  @SuppressWarnings("unchecked")
+  public Fact getFacts(int i) {
+     return (Fact)(((FSArray<Fact>)(_getFeatureValueNc(wrapGetIntCatchException(_FH_Facts)))).get(i));
+  } 
+
+  /** indexed setter for Facts - sets an indexed value - Set of Fact
+   * @generated
+   * @param i index in the array to set
+   * @param v value to set into the array 
+   */
+  @SuppressWarnings("unchecked")
+    public void setFacts(int i, Fact v) {
+    ((FSArray<Fact>)(_getFeatureValueNc(wrapGetIntCatchException(_FH_Facts)))).set(i, v);
+  }  
+  }
+
+    
