2543fc8941c22d6bdea185f08b002df525eb14b3
LeonHammerla <leon.hammerla@gmx.de>
2025-03-28 13:44:27 +0100

    added annis classes

---
+++ src/main/java/org/texttechnologylab/annotation/annis/Variation.java
@@ -0,0 +1,131 @@
+
+
+   
+/* Apache UIMA v3 - First created by JCasGen Fri Mar 28 13:42:33 CET 2025 */
+
+package org.texttechnologylab.annotation.annis;
+ 
+
+import java.lang.invoke.CallSite;
+import java.lang.invoke.MethodHandle;
+
+import org.apache.uima.cas.impl.CASImpl;
+import org.apache.uima.cas.impl.TypeImpl;
+import org.apache.uima.cas.impl.TypeSystemImpl;
+import org.apache.uima.jcas.JCas; 
+import org.apache.uima.jcas.JCasRegistry;
+
+
+
+
+/** 
+ * Updated by JCasGen Fri Mar 28 13:42:33 CET 2025
+ * XML source: /home/staff_homes/lehammer/Documents/work/UIMATypeSystem/target/jcasgen/typesystem.xml
+ * @generated */
+public class Variation extends Annotation {
+ 
+  /** @generated
+   * @ordered 
+   */
+  @SuppressWarnings ("hiding")
+  public final static String _TypeName = "org.texttechnologylab.annotation.annis.Variation";
+  
+  /** @generated
+   * @ordered 
+   */
+  @SuppressWarnings ("hiding")
+  public final static int typeIndexID = JCasRegistry.register(Variation.class);
+  /** @generated
+   * @ordered 
+   */
+  @SuppressWarnings ("hiding")
+  public final static int type = typeIndexID;
+  /** @generated
+   * @return index of the type  
+   */
+  @Override
+  public              int getTypeIndexID() {return typeIndexID;}
+ 
+ 
+  /* *******************
+   *   Feature Offsets *
+   * *******************/ 
+   
+  public final static String _FeatName_layer = "layer";
+
+
+  /* Feature Adjusted Offsets */
+  private final static CallSite _FC_layer = TypeSystemImpl.createCallSite(Variation.class, "layer");
+  private final static MethodHandle _FH_layer = _FC_layer.dynamicInvoker();
+
+   
+  /** Never called.  Disable default constructor
+   * @generated */
+  @Deprecated
+  @SuppressWarnings ("deprecation")
+  protected Variation() {/* intentionally empty block */}
+    
+  /** Internal - constructor used by generator 
+   * @generated
+   * @param casImpl the CAS this Feature Structure belongs to
+   * @param type the type of this Feature Structure 
+   */
+  public Variation(TypeImpl type, CASImpl casImpl) {
+    super(type, casImpl);
+    readObject();
+  }
+  
+  /** @generated
+   * @param jcas JCas to which this Feature Structure belongs 
+   */
+  public Variation(JCas jcas) {
+    super(jcas);
+    readObject();   
+  } 
+
+
+  /** @generated
+   * @param jcas JCas to which this Feature Structure belongs
+   * @param begin offset to the begin spot in the SofA
+   * @param end offset to the end spot in the SofA 
+  */  
+  public Variation(JCas jcas, int begin, int end) {
+    super(jcas);
+    setBegin(begin);
+    setEnd(end);
+    readObject();
+  }   
+
+  /** 
+   * <!-- begin-user-doc -->
+   * Write your own initialization here
+   * <!-- end-user-doc -->
+   *
+   * @generated modifiable 
+   */
+  private void readObject() {/*default - does nothing empty block */}
+     
+ 
+    
+  //*--------------*
+  //* Feature: layer
+
+  /** getter for layer - gets 
+   * @generated
+   * @return value of the feature 
+   */
+  public String getLayer() { 
+    return _getStringValueNc(wrapGetIntCatchException(_FH_layer));
+  }
+    
+  /** setter for layer - sets  
+   * @generated
+   * @param v value to set into the feature 
+   */
+  public void setLayer(String v) {
+    _setStringValueNfc(wrapGetIntCatchException(_FH_layer), v);
+  }    
+    
+  }
+
+    
