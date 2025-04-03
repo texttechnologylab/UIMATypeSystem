b0be84be8087a43318396441d7f1f07c120eac18
Bhuvanesh-Verma <workoholic24x7@gmail.com>
2025-04-03 16:15:57 +0200

    add image caption type system

---
+++ src/main/java/org/texttechnologylab/annotation/Caption.java
@@ -0,0 +1,156 @@
+
+
+   
+/* Apache UIMA v3 - First created by JCasGen Thu Apr 03 16:13:38 CEST 2025 */
+
+package org.texttechnologylab.annotation;
+ 
+
+import java.lang.invoke.CallSite;
+import java.lang.invoke.MethodHandle;
+
+import org.apache.uima.cas.impl.CASImpl;
+import org.apache.uima.cas.impl.TypeImpl;
+import org.apache.uima.cas.impl.TypeSystemImpl;
+import org.apache.uima.jcas.JCas; 
+import org.apache.uima.jcas.JCasRegistry;
+
+
+import org.apache.uima.jcas.tcas.Annotation;
+
+
+/** 
+ * Updated by JCasGen Thu Apr 03 16:13:38 CEST 2025
+ * XML source: /home/staff_homes/verma/Documents/projects/UIMATypeSystem/target/jcasgen/typesystem.xml
+ * @generated */
+public class Caption extends Annotation {
+ 
+  /** @generated
+   * @ordered 
+   */
+  @SuppressWarnings ("hiding")
+  public final static String _TypeName = "org.texttechnologylab.annotation.Caption";
+  
+  /** @generated
+   * @ordered 
+   */
+  @SuppressWarnings ("hiding")
+  public final static int typeIndexID = JCasRegistry.register(Caption.class);
+  /** @generated
+   * @ordered 
+   */
+  @SuppressWarnings ("hiding")
+  public final static int type = typeIndexID;
+  /** @generated
+   * @return index of the type  
+   */
+  @Override
+  public              int getTypeIndexID() {return typeIndexID;}
+ 
+ 
+  /* *******************
+   *   Feature Offsets *
+   * *******************/ 
+   
+  public final static String _FeatName_value = "value";
+  public final static String _FeatName_mediaType = "mediaType";
+
+
+  /* Feature Adjusted Offsets */
+  private final static CallSite _FC_value = TypeSystemImpl.createCallSite(Caption.class, "value");
+  private final static MethodHandle _FH_value = _FC_value.dynamicInvoker();
+  private final static CallSite _FC_mediaType = TypeSystemImpl.createCallSite(Caption.class, "mediaType");
+  private final static MethodHandle _FH_mediaType = _FC_mediaType.dynamicInvoker();
+
+   
+  /** Never called.  Disable default constructor
+   * @generated */
+  @Deprecated
+  @SuppressWarnings ("deprecation")
+  protected Caption() {/* intentionally empty block */}
+    
+  /** Internal - constructor used by generator 
+   * @generated
+   * @param casImpl the CAS this Feature Structure belongs to
+   * @param type the type of this Feature Structure 
+   */
+  public Caption(TypeImpl type, CASImpl casImpl) {
+    super(type, casImpl);
+    readObject();
+  }
+  
+  /** @generated
+   * @param jcas JCas to which this Feature Structure belongs 
+   */
+  public Caption(JCas jcas) {
+    super(jcas);
+    readObject();   
+  } 
+
+
+  /** @generated
+   * @param jcas JCas to which this Feature Structure belongs
+   * @param begin offset to the begin spot in the SofA
+   * @param end offset to the end spot in the SofA 
+  */  
+  public Caption(JCas jcas, int begin, int end) {
+    super(jcas);
+    setBegin(begin);
+    setEnd(end);
+    readObject();
+  }   
+
+  /** 
+   * <!-- begin-user-doc -->
+   * Write your own initialization here
+   * <!-- end-user-doc -->
+   *
+   * @generated modifiable 
+   */
+  private void readObject() {/*default - does nothing empty block */}
+     
+ 
+    
+  //*--------------*
+  //* Feature: value
+
+  /** getter for value - gets Caption text
+   * @generated
+   * @return value of the feature 
+   */
+  public String getValue() { 
+    return _getStringValueNc(wrapGetIntCatchException(_FH_value));
+  }
+    
+  /** setter for value - sets Caption text 
+   * @generated
+   * @param v value to set into the feature 
+   */
+  public void setValue(String v) {
+    _setStringValueNfc(wrapGetIntCatchException(_FH_value), v);
+  }    
+    
+   
+    
+  //*--------------*
+  //* Feature: mediaType
+
+  /** getter for mediaType - gets Type of media (e.g., image, video)
+   * @generated
+   * @return value of the feature 
+   */
+  public String getMediaType() { 
+    return _getStringValueNc(wrapGetIntCatchException(_FH_mediaType));
+  }
+    
+  /** setter for mediaType - sets Type of media (e.g., image, video) 
+   * @generated
+   * @param v value to set into the feature 
+   */
+  public void setMediaType(String v) {
+    _setStringValueNfc(wrapGetIntCatchException(_FH_mediaType), v);
+  }    
+    
+  }
+
+    
