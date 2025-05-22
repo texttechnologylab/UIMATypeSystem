8cfd8c870346b48665281f8dad8e254be50da7d8
bagci <bagci@em.uni-frankfurt.de>
2025-05-22 16:28:56 +0200

    Add new Type for reference text search

---
+++ src/main/java/org/texttechnologylab/type/search/TextSearch.java
@@ -0,0 +1,132 @@
+
+
+   
+/* Apache UIMA v3 - First created by JCasGen Thu May 22 16:17:34 CEST 2025 */
+
+package org.texttechnologylab.type.search;
+ 
+
+import java.lang.invoke.CallSite;
+import java.lang.invoke.MethodHandle;
+
+import org.apache.uima.cas.impl.CASImpl;
+import org.apache.uima.cas.impl.TypeImpl;
+import org.apache.uima.cas.impl.TypeSystemImpl;
+import org.apache.uima.jcas.JCas; 
+import org.apache.uima.jcas.JCasRegistry;
+
+
+import org.apache.uima.jcas.tcas.Annotation;
+
+
+/** Text, which is used for searching in the web, for Reference Text or for other purposes.
+ * Updated by JCasGen Thu May 22 16:17:34 CEST 2025
+ * XML source: /home/staff_homes/bagci/projects/UIMATypeSystem/target/jcasgen/typesystem.xml
+ * @generated */
+public class TextSearch extends Annotation {
+ 
+  /** @generated
+   * @ordered 
+   */
+  @SuppressWarnings ("hiding")
+  public final static String _TypeName = "org.texttechnologylab.type.search.TextSearch";
+  
+  /** @generated
+   * @ordered 
+   */
+  @SuppressWarnings ("hiding")
+  public final static int typeIndexID = JCasRegistry.register(TextSearch.class);
+  /** @generated
+   * @ordered 
+   */
+  @SuppressWarnings ("hiding")
+  public final static int type = typeIndexID;
+  /** @generated
+   * @return index of the type  
+   */
+  @Override
+  public              int getTypeIndexID() {return typeIndexID;}
+ 
+ 
+  /* *******************
+   *   Feature Offsets *
+   * *******************/ 
+   
+  public final static String _FeatName_text = "text";
+
+
+  /* Feature Adjusted Offsets */
+  private final static CallSite _FC_text = TypeSystemImpl.createCallSite(TextSearch.class, "text");
+  private final static MethodHandle _FH_text = _FC_text.dynamicInvoker();
+
+   
+  /** Never called.  Disable default constructor
+   * @generated */
+  @Deprecated
+  @SuppressWarnings ("deprecation")
+  protected TextSearch() {/* intentionally empty block */}
+    
+  /** Internal - constructor used by generator 
+   * @generated
+   * @param casImpl the CAS this Feature Structure belongs to
+   * @param type the type of this Feature Structure 
+   */
+  public TextSearch(TypeImpl type, CASImpl casImpl) {
+    super(type, casImpl);
+    readObject();
+  }
+  
+  /** @generated
+   * @param jcas JCas to which this Feature Structure belongs 
+   */
+  public TextSearch(JCas jcas) {
+    super(jcas);
+    readObject();   
+  } 
+
+
+  /** @generated
+   * @param jcas JCas to which this Feature Structure belongs
+   * @param begin offset to the begin spot in the SofA
+   * @param end offset to the end spot in the SofA 
+  */  
+  public TextSearch(JCas jcas, int begin, int end) {
+    super(jcas);
+    setBegin(begin);
+    setEnd(end);
+    readObject();
+  }   
+
+  /** 
+   * <!-- begin-user-doc -->
+   * Write your own initialization here
+   * <!-- end-user-doc -->
+   *
+   * @generated modifiable 
+   */
+  private void readObject() {/*default - does nothing empty block */}
+     
+ 
+    
+  //*--------------*
+  //* Feature: text
+
+  /** getter for text - gets Can be Same as covered text or a summary, topic, etc. of this text
+   * @generated
+   * @return value of the feature 
+   */
+  public String getText() { 
+    return _getStringValueNc(wrapGetIntCatchException(_FH_text));
+  }
+    
+  /** setter for text - sets Can be Same as covered text or a summary, topic, etc. of this text 
+   * @generated
+   * @param v value to set into the feature 
+   */
+  public void setText(String v) {
+    _setStringValueNfc(wrapGetIntCatchException(_FH_text), v);
+  }    
+    
+  }
+
+    
