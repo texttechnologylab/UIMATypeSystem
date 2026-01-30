a64b9062daec6ac2191d3e561eb7f9063e51c7fa
Giuseppe Abrami <abrami@em.uni-frankfurt.de>
2026-01-30 15:18:27 +0100

    small updates for AnnotationSchema

--- src/main/java/org/texttechnologylab/annotation/schema/AnnotationRelation.java
+++ src/main/java/org/texttechnologylab/annotation/schema/AnnotationRelation.java
@@ -1,28 +1,26 @@
 
 
    
-/* Apache UIMA v3 - First created by JCasGen Thu Oct 30 18:05:17 CET 2025 */
+/* Apache UIMA v3 - First created by JCasGen Fri Jan 30 15:15:29 CET 2026 */
 
 package org.texttechnologylab.annotation.schema;
- 
 
-import java.lang.invoke.CallSite;
-import java.lang.invoke.MethodHandle;
 
 import org.apache.uima.cas.impl.CASImpl;
 import org.apache.uima.cas.impl.TypeImpl;
 import org.apache.uima.cas.impl.TypeSystemImpl;
-import org.apache.uima.jcas.JCas; 
+import org.apache.uima.jcas.JCas;
 import org.apache.uima.jcas.JCasRegistry;
-
-
-import org.apache.uima.jcas.cas.FSList;
+import org.apache.uima.jcas.cas.FSArray;
 import org.apache.uima.jcas.cas.TOP;
 
+import java.lang.invoke.CallSite;
+import java.lang.invoke.MethodHandle;
 
-/** 
- * Updated by JCasGen Thu Oct 30 18:05:17 CET 2025
- * XML source: /nvme/projects/TTLab/UIMATypeSystem/target/jcasgen/typesystem.xml
+
+/**
+ * Updated by JCasGen Fri Jan 30 15:15:29 CET 2026
+ * XML source: /home/staff_homes/abrami/Projects/GitProjects/UIMATypeSystem/target/jcasgen/typesystem.xml
  * @generated */
 public class AnnotationRelation extends AnnotationAttribute {
  
@@ -105,18 +103,42 @@ public class AnnotationRelation extends AnnotationAttribute {
    * @return value of the feature 
    */
   @SuppressWarnings("unchecked")
-  public FSList<TOP> getAttributes() { 
-    return (FSList<TOP>)(_getFeatureValueNc(wrapGetIntCatchException(_FH_attributes)));
+  public FSArray<TOP> getAttributes() {
+      return (FSArray<TOP>) (_getFeatureValueNc(wrapGetIntCatchException(_FH_attributes)));
   }
     
   /** setter for attributes - sets  
    * @generated
    * @param v value to set into the feature 
    */
-  public void setAttributes(FSList<TOP> v) {
+  public void setAttributes(FSArray<TOP> v) {
     _setFeatureValueNcWj(wrapGetIntCatchException(_FH_attributes), v);
-  }    
-    
+  }
+
+
+    /**
+     * indexed getter for attributes - gets an indexed value -
+     *
+     * @param i index in the array to get
+     * @return value of the element at index i
+     * @generated
+     */
+    @SuppressWarnings("unchecked")
+    public TOP getAttributes(int i) {
+        return (TOP) (((FSArray<TOP>) (_getFeatureValueNc(wrapGetIntCatchException(_FH_attributes)))).get(i));
+    }
+
+    /**
+     * indexed setter for attributes - sets an indexed value -
+     *
+     * @param i index in the array to set
+     * @param v value to set into the array
+     * @generated
+     */
+    @SuppressWarnings("unchecked")
+    public void setAttributes(int i, TOP v) {
+        ((FSArray<TOP>) (_getFeatureValueNc(wrapGetIntCatchException(_FH_attributes)))).set(i, v);
+    }
   }
 
     
